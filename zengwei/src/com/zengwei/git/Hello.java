10b073c3785544e5fdcb03876f35a3a01674c3ae
zengwei <945205147@qq.com>
2019-11-28 22:57:05 +0800

    2.0

--- zengwei/src/com/zengwei/git/Hello.java
+++ zengwei/src/com/zengwei/git/Hello.java
@@ -3,6 +3,13 @@ package com.zengwei.git;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello git");
+
+
+    }
+
+    public void test1() {
+       int i = 1;
+        System.out.println(i);
     }
 
 }
